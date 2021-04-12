f1012378cda368f6d613801f4bb8021cfb4b8224
sunbingteng <2159869049@qq.com>
2021-04-12 14:40:31 +0800

    first

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+from flask import Flask#从flask包中导入Flask类
+
+app = Flask(__name__)#将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
+
+from app import routes#从app包中导入模块routes
---
+++ microblog.py
@@ -0,0 +1,5 @@
+from app import app
+
+if __name__ == '__main__':
+    print('123')
+    app.run(debug=True)
